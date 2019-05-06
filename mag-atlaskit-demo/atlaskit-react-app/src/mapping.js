f65a38cb56cca2d7269fa8b9d8fd748dbc53abea
Christopher Zimmermann <topher@dandelion.org>
2019-05-06 07:51:48 +0200

    Added button and date-picker components.

--- mag-atlaskit-demo/atlaskit-react-app/src/mapping.js
+++ mag-atlaskit-demo/atlaskit-react-app/src/mapping.js
@@ -1,7 +1,11 @@
 
 import PageTitleWrapper from "./components/PageTitleWrapper";
+import Button from '@atlaskit/button';
+import {DatePicker} from '@atlaskit/datetime-picker';
 
 const COMPONENTS = {
+  "atlaskit:components/button": Button,
+  "atlaskit:components/date-picker": DatePicker,
   "atlaskit:components/page-title": PageTitleWrapper
 };
 
